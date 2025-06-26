c98db58eef2291d798e3a4a63810f456bcdf6ebf
wtfoong <wtfoong81@gmail.com>
2025-06-26 15:58:20 +0800

    New Update

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from datetime import datetime
 import time
+import pandas as pd
 
 # import local module
 from config_loader import load_config
 config = load_config()
 
-from backend import load_data, load_data_all, get_inspection_data
+from backend import load_data, load_data_all, get_inspection_data, get_CTQ_SpecNo
 from helper import set_timer_style, plot_IMR, calculate_ppk
 
 # ---- Load app setting from config ----
@@ -32,6 +33,11 @@ def get_inspection_data_cached(sapcode, specno):
     df_inspection_data = get_inspection_data(sapcode, specno)
     return df_inspection_data
 
+@st.cache_data(ttl= INSPECTION_DATA_CACHE)
+def get_CTQ_SpecNo_cached(sapcode):
+    df_inspection_data = get_CTQ_SpecNo(sapcode)
+    return df_inspection_data
+
 # ---- UI ----
 # ---- Page config ----
 page_title = config['app']['title']
@@ -39,7 +45,7 @@ st.set_page_config(page_title=page_title, layout="wide")
 
 #! deprecating! UI flickers and lost session state...(filter selection etc gone...)
 # html meta tags to refresh at browser level
-st.markdown(f'<meta http-equiv="refresh" content="{PAGE_REFRESH}">',unsafe_allow_html=True)
+#st.markdown(f'<meta http-equiv="refresh" content="{PAGE_REFRESH}">',unsafe_allow_html=True)
 
 # header
 st.markdown(
@@ -54,38 +60,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-df_tool_data, df_tool_data_all, last_refresh = load_data_cached()
 
-st.markdown(f"<p style='text-align: center; color: grey;'>Last refreshed: {last_refresh}</p>", unsafe_allow_html=True)
-
-#! ==================== WIP --> STILL BUGGY!!
-# # ---- Auto rerun every PAGE_REFRESH seconds (without clearing session state) ----
-
-# if 'enable_autorefresh' not in st.session_state:
-#     st.session_state.enable_autorefresh = True
-
-# st.checkbox("Enable Auto Refresh", key='enable_autorefresh') #! value = True??
-
-# if st.session_state.enable_autorefresh:
-#     with st.empty():
-#         for i in range(PAGE_REFRESH, 0, -1):
-#             st.markdown(f"<p style='text-align:center; color:grey;'>⏳ Auto-refresh in {i}s</p>", unsafe_allow_html=True)
-#             time.sleep(1)
-#         st.rerun()
-#! ====================
-
-# ---- Filters ----
-with st.container():
-    col1, col2, col3 = st.columns(3)
-
-    with col2:
-        location_options = list(df_tool_data["Location"].unique())
-        selected_locations = st.multiselect(label = ' ', label_visibility='collapsed', options=location_options, placeholder='Choose Machine')
-
-filtered_df = df_tool_data.copy()
-
-if selected_locations:
-    filtered_df = filtered_df[filtered_df["Location"].isin(selected_locations)]
 
 
 # ---- Initialize state ----
@@ -99,114 +74,187 @@ if 'clicked_location' not in st.session_state:
 
 # ---- Information Display ----
 
-# Tooling countdown times
-with st.container():
-    col1, col2, col3 = st.columns([1,4,1])
+@st.fragment(run_every=str(PAGE_REFRESH)+"s")
+def ShowTimerInfo():
+    df_tool_data, df_tool_data_all, last_refresh = load_data_cached()
 
-    with col2:
-        for index, row in filtered_df.iterrows():
 
-            # Create 3 columns: machine name | timer | button
-            col_name, col_timer, col_tool, col_button = st.columns([3, 2, 1, 1])  # adjust ratios as needed
+    #! ==================== WIP --> STILL BUGGY!!
+    # # ---- Auto rerun every PAGE_REFRESH seconds (without clearing session state) ----
 
-            with col_name:
-                if row['TechRequired']:
-                    st.markdown(f"<div style='font-size: 50px;animation: blinker 1s linear infinite;'><strong>{row['Location']} 🧑‍🏭</strong></div>", unsafe_allow_html=True)
-                else:
-                    st.markdown(f"<div style='font-size: 50px;'><strong>{row['Location']}</strong></div>", unsafe_allow_html=True)
+    # if 'enable_autorefresh' not in st.session_state:
+    #     st.session_state.enable_autorefresh = True
 
-            with col_timer:
-                color, blink_style = set_timer_style(row['DurationMins'])
+    # st.checkbox("Enable Auto Refresh", key='enable_autorefresh') #! value = True??
 
-                st.markdown(
-                    f"""
-                    <style>
-                        @keyframes blinker {{
-                            50% {{ opacity: 0; }}
-                        }}
-                    </style>
-                    <div style="color: {color}; font-size: 50px; {blink_style}">
-                        {row['DurationMins']} mins
-                    </div>
-                    """,
-                    unsafe_allow_html=True,
-                )
+    # if st.session_state.enable_autorefresh:
+    #     with st.empty():
+    #         for i in range(PAGE_REFRESH, 0, -1):
+    #             st.markdown(f"<p style='text-align:center; color:grey;'>⏳ Auto-refresh in {i}s</p>", unsafe_allow_html=True)
+    #             time.sleep(1)
+    #         st.rerun()
+    #! ====================
 
-            with col_tool:
-                st.markdown("<div style='height:25px;'></div>", unsafe_allow_html=True)  # Top spacer
+    # ---- Filters ----
+    with st.container():
+        col1, col2, col3 = st.columns(3)
 
-                # Store selected location for showing details at bottom section
-                if st.button("Show 🛠️", key=f"btn_{row['Location']}", use_container_width=True):
-                    #toggle off
-                    if st.session_state.clicked_location == row['Location']:
-                        st.session_state.clicked_location = None # clear session state
-                    #toggle on
+        with col2:
+            location_options = list(df_tool_data["Location"].unique())
+            selected_locations = st.multiselect(label = ' ', label_visibility='collapsed', options=location_options, placeholder='Choose Machine')
+
+    filtered_df = df_tool_data.copy()
+
+    if selected_locations:
+        filtered_df = filtered_df[filtered_df["Location"].isin(selected_locations)]
+    
+    st.markdown(f"<p style='text-align: center; color: grey;'>Last refreshed: {last_refresh}</p>", unsafe_allow_html=True)
+    with st.container():
+        col1, col2, col3 = st.columns([1,4,1])
+
+        with col2:
+            for index, row in filtered_df.iterrows():
+
+                # Create 3 columns: machine name | timer | button
+                col_name, col_timer, col_tool, col_button, col_LED = st.columns([3, 2, 1, 1,1])  # adjust ratios as needed
+
+                with col_name:
+                    if row['TechRequired']:
+                        st.markdown(f"<div style='font-size: 50px;animation: blinker 1s linear infinite;'><strong>{row['Location']} 🧑‍🏭</strong></div>", unsafe_allow_html=True)
                     else:
+                        st.markdown(f"<div style='font-size: 50px;'><strong>{row['Location']}</strong></div>", unsafe_allow_html=True)
+
+                with col_timer:
+                    color, blink_style = set_timer_style(row['DurationMins'])
+
+                    st.markdown(
+                        f"""
+                        <style>
+                            @keyframes blinker {{
+                                50% {{ opacity: 0; }}
+                            }}
+                        </style>
+                        <div style="color: {color}; font-size: 50px; {blink_style}">
+                            {row['DurationMins']} mins
+                        </div>
+                        """,
+                        unsafe_allow_html=True,
+                    )
+
+                with col_tool:
+                    st.markdown("<div style='height:25px;'></div>", unsafe_allow_html=True)  # Top spacer
+
+                    # Store selected location for showing details at bottom section
+                    if st.button("Show 🛠️", key=f"btn_{row['Location']}", use_container_width=True):
+                        # #toggle off
+                        # if st.session_state.clicked_location == row['Location']:
+                        #     st.session_state.clicked_location = None # clear session state
+                        # #toggle on
+                        # else:
                         st.session_state.clicked_location = row['Location'] # update session state
 
                         st.session_state.clicked_materialcode = None  # 👈 force close the clicked_materialcode button
 
-            with col_button:
-                st.markdown("<div style='height:25px;'></div>", unsafe_allow_html=True)  # Top spacer
+                with col_button:
+                    st.markdown("<div style='height:25px;'></div>", unsafe_allow_html=True)  # Top spacer
 
-                # Store selected materialcode for plotting at bottom section
-                if st.button("Show 📈", key=f"btn_{row['MaterialCode']}", use_container_width=True):
-                    #toggle off
-                    if st.session_state.clicked_materialcode == row['MaterialCode']:
-                        st.session_state.clicked_materialcode = None # clear session state
-                    #toggle on
-                    else:
+                    # Store selected materialcode for plotting at bottom section
+                    if st.button("Show 📈", key=f"btn_{row['MaterialCode']}", use_container_width=True):
+                        # #toggle off
+                        # if st.session_state.clicked_materialcode == row['MaterialCode']:
+                        #     st.session_state.clicked_materialcode = None # clear session state
+                        # #toggle on
+                        # else:
                         st.session_state.clicked_materialcode = row['MaterialCode'] # update session state
 
                         st.session_state.clicked_location = None  # 👈 force close the clicked_location button
+                with col_LED:
+                    
+                    color = (
+                        'red' if row['MacLEDRed'] else
+                        'yellow' if row['MacLEDYellow'] else
+                        '#00FF00' if row['MacLEDGreen'] else
+                        'None'
+                    )
+                    st.markdown(f"""
+                        <style>
+                            .circle-container {{
+                                    display: flex;
+                                    align-items: center;
+                                    justify-content: flex-end;
+                                    height: 100px; /* Adjust height as needed */
+                            }}
+                            .circle-button {{
+                                    height: 30px;
+                                    width: 30px;
+                                    border-radius: 50%;
+                                    border: 1px solid #000;
+                                    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3);
+                            }}
+                        </style>
+                        <div class="circle-container">
+                            <span class="circle-button" style=" background: {color};"></span>
+                        </div>
+                        """, unsafe_allow_html=True)
 
-# ---- Bottom Section: Show tool data for clicked_location ----
-with st.container():
-    col1, col2, col3 = st.columns([1,4,1])
 
-    with col2:
-        def clear_selection_clicked_location():
-            st.session_state.clicked_location = None
 
-        if st.session_state.clicked_location:
-            st.markdown('---')
-            st.markdown("### 📋 Upcoming Tool Change")
-            st.info(f"Showing data for: `{st.session_state.clicked_location}`")
+    # ---- Bottom Section: Show tool data for clicked_location ----
+    with st.container():
+        col1, col2, col3 = st.columns([1,4,1])
 
-            cols = ['Turret','Tool','Process','Balance (mins)', 'Balance (pcs)']
-            df = df_tool_data_all[df_tool_data_all['Location']==st.session_state.clicked_location]
-            df = df[cols]
+        with col2:
+            def clear_selection_clicked_location():
+                st.session_state.clicked_location = None
 
-            st.dataframe(df, hide_index= True, use_container_width = False)
-            st.button("❌ Close",key = f'close_{st.session_state.clicked_location}' , on_click=clear_selection_clicked_location)
-            st.markdown('---')
+            if st.session_state.clicked_location:
+                st.markdown('---')
+                st.markdown("### 📋 Upcoming Tool Change")
+                st.info(f"Showing data for: `{st.session_state.clicked_location}`")
 
-# ---- Bottom Section: Show IMR Chart for clicked_materialcode ----
-with st.container():
-    col1, col2, col3 = st.columns([1,4,1])
+                cols = ['Turret','Tool','Process','Balance (mins)', 'Balance (pcs)']
+                df = df_tool_data_all[df_tool_data_all['Location']==st.session_state.clicked_location]
+                df = df[cols]
 
-    with col2:
-        def clear_selection_clicked_materialcode():
-            st.session_state.clicked_materialcode = None
+                st.dataframe(df, hide_index= True, use_container_width = False)
+                st.button("❌ Close",key = f'close_{st.session_state.clicked_location}' , on_click=clear_selection_clicked_location)
+                st.markdown('---')
 
-        if st.session_state.clicked_materialcode:
-            st.markdown('---')
-            st.markdown("### 🔍 Inspection Details")
+    # ---- Bottom Section: Show IMR Chart for clicked_materialcode ----
+    with st.container():
+        col1, col2, col3 = st.columns([1,4,1])
 
-            materialcode = st.session_state.clicked_materialcode
-            specno = '201' #! hardcoded specno
+        with col2:
+            def clear_selection_clicked_materialcode():
+                st.session_state.clicked_materialcode = None
 
-            df_inspection_data = get_inspection_data_cached(materialcode, specno)
+            if st.session_state.clicked_materialcode:
+                st.markdown('---')
+                st.markdown("### 🔍 Inspection Details")
 
-            if not df_inspection_data.empty:
-                # Calculate ppk
-                ppk = calculate_ppk(df_inspection_data['MeasVal'],62.6,62.5) #! todo hardcoded USL LSL
+                materialcode = st.session_state.clicked_materialcode
+                specnoList = get_CTQ_SpecNo_cached(materialcode)
+                #specno = '201' #! hardcoded specno
+                for specno in specnoList['BalloonNo'].unique():
+                    df_inspection_data = get_inspection_data_cached(materialcode, specno)
 
-                st.info(f"Showing details for: `{st.session_state.clicked_materialcode} | Ø 62.6 +0/-0.1 | Ppk = {ppk}`") #! todo hardcoded DIMENSION DESCR
-                fig = plot_IMR(df_inspection_data,62.6,62.5)  #! todo hardcoded USL LSL
-                st.pyplot(fig)
-            else:
-                st.warning(f"No inspection data available for `{st.session_state.clicked_materialcode}`.")
+                    if not df_inspection_data.empty:
+                        # Calculate ppk
+                        df_inspection_data['MinVal'] = pd.to_numeric(df_inspection_data['MinVal'], errors='coerce')
+        
+                        df_inspection_data['MaxVal'] = pd.to_numeric(df_inspection_data['MaxVal'], errors='coerce')
 
-            st.button("❌ Close",key = f'close_{st.session_state.clicked_materialcode}', on_click=clear_selection_clicked_materialcode)
-            st.markdown('---')
+                        ppk = calculate_ppk(df_inspection_data['MeasVal'],df_inspection_data['MinVal'].iloc[0],df_inspection_data['MaxVal'].iloc[0])
+
+                        st.info(f"Showing details for: `{st.session_state.clicked_materialcode} |SpecNo:{specno}| {df_inspection_data['Description'].iloc[0]} | Ppk = {ppk}`")
+                        fig = plot_IMR(df_inspection_data,df_inspection_data['MinVal'].iloc[0],df_inspection_data['MaxVal'].iloc[0]) 
+                        st.pyplot(fig)
+                    else:
+                        st.warning(f"No inspection data available for `{st.session_state.clicked_materialcode}`.")
+
+                st.button("❌ Close",key = f'close_{st.session_state.clicked_materialcode}', on_click=clear_selection_clicked_materialcode)
+                st.markdown('---')
+                
+ShowTimerInfo()
+
+# Tooling countdown times
